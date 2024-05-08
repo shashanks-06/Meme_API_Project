cc6ceb308a9c3aef2b08eeb4055e67f5597fd1a5
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-05-08 23:41:05 +0530

    successfull fetching

---
+++ script.js
@@ -0,0 +1,24 @@
+const memeBtn = document.querySelector("#generate-meme");
+const memeTitle = document.querySelector(".meme-title");
+const memeImage = document.querySelector(".meme-image");
+const authorOutput = document.querySelector(".author span");
+
+function getMeme() {
+  fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then((data) => {
+      const { author, title, url } = data;
+
+      authorOutput.innerText = `Meme By: ${author}`;
+      memeTitle.innerText = title;
+      memeImage.src = url;
+
+      console.log({ author, title, url });
+    });
+}
+
+getMeme();
+
+memeBtn.addEventListener("click", () => {
+  getMeme();
+});
